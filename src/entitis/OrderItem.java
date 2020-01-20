870aa9ae87425ec221db9f8523363b3ae793dc16
Claudio Filho <brosson123@gmail.com>
2020-01-20 17:27:56 -0300

    Projeto Finalizado

---
+++ src/entitis/OrderItem.java
@@ -0,0 +1,54 @@
+package entitis;
+
+public class OrderItem {
+
+	private Integer quantity;
+	private Double price;
+	private Product product;
+	
+	public OrderItem() {
+	}
+	
+	public OrderItem(int quantity, double price, Product product) {
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	public Double subTotal() {
+		return price*quantity;
+	}
+	
+	public String toString() {
+		StringBuilder sc = new StringBuilder();
+		sc.append(product.getName()+", ");
+		sc.append(String.format("$"+"%.2f",product.getPrice())+", Quantity: ");
+		sc.append(quantity+ ", Subtotal: $");
+		sc.append(String.format("%.2f",subTotal()));
+		return sc.toString();
+	}
+}
---
+++ src/entitis/enu/OrderStatus.java
@@ -0,0 +1,10 @@
+package entitis.enu;
+
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+	
+}
